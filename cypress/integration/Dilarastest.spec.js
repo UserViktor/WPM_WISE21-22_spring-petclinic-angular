b3a18ce57193da9991e4b94e4ac92851ea11dab9
Verpixelt <d.kara@live.de>
2022-01-09 15:36:57 +0100

    Cypressfil erstellt

---
+++ cypress/integration/Dilarastest.spec.js
@@ -0,0 +1,18 @@
+
+
+
+context('Ownerloeschen', () => {
+
+  it('Owner Seite soll besucht werden können', ()=> {
+    cy.visit('http://localhost:4200/petclinic/owners')
+  })
+
+
+  /*it('Owner soll gelöscht werden', ()=>{
+    cy.get('#ownersTable').get('div')
+   // cy.find('Add Owner')
+  }) */
+
+
+
+})
