6f6e055992a4827b6b50187f17cc86e288dbc788
Siincy <46598346+UserViktor@users.noreply.github.com>
2022-02-09 16:01:18 +0100

    fix merge conflict

---
+++ cypress/integration/AddVisit.js
@@ -0,0 +1,18 @@
+context('Find', () => {
+
+
+
+  it('Eine Owner Seite rufen', () => {
+    cy.visit('http://localhost:4200/petclinic/owners/1')
+  })
+
+
+  it('Visit hinzufügen', () => {
+    cy.get('#addVisitButton').click();
+    cy.get('#kalender').type('06/02.2020');
+    cy.get('#description').type('Bespiel Termin für Pet');
+    cy.get('button[type=submit]').as('Add Visit').click();
+  })
+
+
+})
